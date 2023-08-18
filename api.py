f24ca60a121c11bd9b74230e51331b3dc867178c
Mateusz Witka-Jeżewski <m.w.jezewski@gmail.com>
2023-08-18 21:33:03 +0200

    api works

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import uvicorn
 import json
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+
+from utils.api_packer import ApiPacker
 
 app = FastAPI()
 
@@ -12,23 +14,26 @@ async def root():
     return {"message": "TEST_API"}
 
 
-class Point(BaseModel):
-    x: float
-    y: float
-    z: float
+class Cuboid(BaseModel):
+    width: float
+    height: float
+    depth: float
 
 
 class RequestBody(BaseModel):
-    points: list[Point]
+    cuboids: list[Cuboid]
 
 @app.post("/packit")
 async def pack_cubes(request_data: RequestBody):
-    points = request_data.points
-    print(points)
-    with open("data/test_api_data.json", "r") as f:
-        data = json.load(f)
-    return data
-
+    print(request_data)
+    cuboids = request_data.cuboids
+    if cuboids is None:
+        raise HTTPException(status_code=400, detail="No cuboids provided")
+    api_packer = ApiPacker(cuboids)
+    result = api_packer.pack_cuboids()
+    if result is  None:
+        raise HTTPException(status_code=500, detail="Can't pack the cuboids")
+    return result
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
